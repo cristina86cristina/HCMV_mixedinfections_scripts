19e31e5dca67ae4f81be51878ba82c4d2b856a8b
cristina86cristina <cristina.venturini.11@gmail.com>
2022-07-19 14:02:49 +0100

    Update harold step1

--- submit_harold_parameters_cmv.sh
+++ submit_harold_parameters_cmv.sh
@@ -1,28 +1,27 @@
-for n_haplo in {2..2}; do
+for n_haplo in {2..3}; do
     echo $n_haplo
 sample=R02-00180
-mkdir /cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection/${sample}/harold
-mkdir /cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection/${sample}/harold/nhaplo/
-mkdir /cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection/${sample}/harold/nhaplo/cluster
-mkdir /cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection/${sample}/harold/nhaplo/cluster/error
-mkdir /cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection/${sample}/harold/nhaplo/cluster/out
-
-mydir=/cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection/${sample}/harold/nhaplo/
+mydir=/cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection/${sample}
 filelist=/cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection/${sample}/samples.txt
 log_file=nhaplo_${n_haplo}_harold_decideparameters.log
 results=nhaplo_${n_haplo}_results
+HAROLD=/SAN/breuerlab/pathseq1/Cristina_home/src/HaROLD-2.0.0/jar/Cluster_RG/dist/Cluster_RG.jar
+
+mkdir ${mydir}/harold
+mkdir ${mydir}/harold/cluster
+
 
 echo "
 
 
 #!/bin/bash -l
 #$ -S /bin/bash
-#$ -o /cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection/${sample}/harold/nhaplo/cluster/out
-#$ -e /cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection/${sample}/harold/nhaplo/cluster/error
+#$ -o ${mydir}/cluster/Haplo_n${n_haplo}.out
+#$ -e ${mydir}/cluster/Haplo_n${n_haplo}.error
 #$ -l h_rt=12:00:00
 #$ -l tmem=20.9G,h_vmem=20.9G
 #$ -N  haplo_${n_haplo}
-#$ -wd  /cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection
+#$ -wd ${mydir}
 #$ -V
 #$ -R y
 
@@ -32,15 +31,15 @@ echo "
 #even before step1, need to decide how many haplotypes: run harold with several n of haplo and get elbow number
 
 source /share/apps/anaconda/bin/activate harold
-cd ${mydir}//
+cd ${mydir}/harold
 
-java -Xmx10g -Xms10g -jar /SAN/breuerlab/Juanita_Virus/Apr2020-Harold-Refinement-Diversity/jar/Cluster_RG/dist/Cluster_RG.jar --count-file ${filelist} --haplotypes ${n_haplo} --alpha-frac 0.1 --gamma-cache 10000 -H -L --threads 1 -p ${mydir}/${results} --seed 12345 > ${mydir}/${log_file}
+java -Xmx10g -Xms10g -jar ${HAROLD} --count-file ${filelist} --haplotypes ${n_haplo} --alpha-frac 0.1 --gamma-cache 10000 -H -L --threads 1 -p ${mydir}/harold/${results} --seed 12345 > ${mydir}/harold/${log_file}
 
 
 
-" > /cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection/${sample}/harold/nhaplo/cluster/nhaplo_${n_haplo}_getparameters.sh
+" > ${mydir}/harold/cluster/nhaplo_${n_haplo}_getparameters.sh
 
-     qsub /cluster/scratch8b/test_CV/CMV_Wellcome/mixed_infection/${sample}/harold/nhaplo/cluster/nhaplo_${n_haplo}_getparameters.sh
+     qsub ${mydir}/harold/cluster/nhaplo_${n_haplo}_getparameters.sh
 
 done
 
